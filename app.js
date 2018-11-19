54cc18be55a2359f59510a5396f4276a842eeeba
Nicolas <nicolasecolman@gmail.com>
2018-11-19 11:33:09 -0300

    Code cleaning

--- app.js
+++ app.js
@@ -1,38 +1,35 @@
 angular
   .module('app', [])
-  .factory('webdb', function(){
+  .factory('webdb', function () {
 
 	var webdb = {};
 	webdb.db = null;
 	webdb.supported = true;
 
-	// Método para crear la base de datos
-	webdb.open = function(options) 
+	webdb.open = function (options) 
 	{
 		if (typeof openDatabase == "undefined") {
 			webdb.supported = false;
 			return;
 		}
 
-		// Opciones por defecto
+		// Default options
 		var options         = options || {};
 		options.name        = options.name || 'noname';
 		options.mb          = options.mb || 5;
 		options.description = options.description || 'no description';
 		options.version     = options.version || '1.0';
 
-		// Definimos el tamaño en MB
+		// Size of the database in MB
 		var dbSize = options.mb * 1024 * 1024;
 
-		// Cargamos la base de datos
 		webdb.db = openDatabase(options.name, options.version, options.description, dbSize);
 	}
 
-	// Método para ejecutar consulta
-	webdb.executeSql = function(sql, data, onSuccess, onError)
+	webdb.executeSql = function (sql, data, onSuccess, onError)
 	{
 		if (!webdb.db) return;
-		webdb.db.transaction(function(tx){
+		webdb.db.transaction(function (tx) {
 			tx.executeSql(sql, data, onSuccess, onError);
 		});
 	};
@@ -43,28 +40,26 @@ angular
 		version: "1.0"
 	};
 
-	// Abrimos la base de datos
 	webdb.open(opt);
 
 	function tratarError(e)
 	{
 		throw "Error al crear base de datos: " + e;
 	}
-	
-	// Creamos la tabla (si no existe)
+
 	webdb.executeSql('CREATE TABLE IF NOT EXISTS nota (id INTEGER PRIMARY KEY ASC, texto TEXT, added_on DATETIME)', [],	function(tx, r){}, tratarError);
 	
 	/**
 	 * Searchs notes in the database
 	 * @returns {Promise} (SQLResultSetRowList)
 	 */
-	webdb.getNotes = function() {
-		return new Promise(function(resolve, reject) {
+	webdb.getNotes = function () {
+		return new Promise(function (resolve, reject) {
 			webdb.executeSql('SELECT * FROM nota order by id desc', [], 
-				function(tx, r) {
+				function (tx, r) {
 					resolve(r.rows);
 				},
-				function() {
+				function () {
 					reject('There was an error.');
 				}
 			);
@@ -77,27 +72,27 @@ angular
 	 * @param {string} _texto 
 	 * @returns {Promise}
 	 */
-	webdb.saveNote = function(_id, _texto) {
-		return new Promise(function(resolve, reject) {			
+	webdb.saveNote = function (_id, _texto) {
+		return new Promise(function (resolve, reject) {			
 			if(_id == "" || _id == undefined){
 				webdb.executeSql('INSERT INTO nota (texto, added_on) VALUES (?,?)', [_texto, new Date()],
-					function(tx, r) {
+					function (tx, r) {
 						resolve();
 					},
-					function() {
+					function () {
 						reject('There was an error.');
 					}
 				);
 			}else{	
 				webdb.executeSql('UPDATE nota SET texto = ? WHERE id = ?', [_texto, _id],
-					function(tx, r) {
+					function (tx, r) {
 						resolve();
 					},
-					function() {
+					function () {
 						reject('There was an error.');
 					}
 				);	
-			}					
+			}	
 		});
 	};
 
@@ -106,55 +101,46 @@ angular
 	 * @param {integer} _id 
 	 * @returns {Promise}
 	 */
-	webdb.deleteNote = function(_id) {
-		return new Promise(function(resolve, reject) {
+	webdb.deleteNote = function (_id) {
+		return new Promise(function (resolve, reject) {
 			webdb.executeSql('delete from nota where id = ?', [_id], resolve, reject);
 		});
 	};
 
 	return webdb;
   })
-  .controller('homeController', function(webdb, $log, $scope){
+  .controller('homeController', function (webdb, $log, $scope) {
 
 	$scope.databaseSupport = webdb.supported;
 	$scope.notes = [];
 
-	$scope.save = function(){
+	$scope.save = function () {
 		var _id = $scope.note.id;
-		var _texto = $scope.note.texto; 
+		var _texto = $scope.note.texto;
 
-		webdb.saveNote(_id, _texto).then(function() { 
+		webdb.saveNote(_id, _texto).then(function () { 
 			$scope.note = undefined; 
 			load(); 
 		}, tratarError);
 
 	}
 
-	$scope.delete = function(note){
+	$scope.delete = function (note) {
     	var id = note.id;
 		if(!confirm("¿Confirma el borrado de la nota?")) return; 
 
 		webdb.deleteNote(id).then(load, tratarError);
     }
 
-	$scope.view = function(note){
-		//Busco texto de la nota
-		/*webdb.executeSql('SELECT texto FROM  nota where id = ?', [id],
-			function(tx, r){
-				var _item = r.rows.item(0);
-				document.getElementById("id").value = id;
-				document.getElementById("texto").value = _item.texto;
-			},
-			tratarError
-		); */
+	$scope.view = function (note) {
 		$scope.note = angular.copy(note);
 	} 
 
-	function load(){
+	function load() {
 
     	$scope.notes = [];
 
-		webdb.getNotes().then(function(rows){ 
+		webdb.getNotes().then(function (rows) { 
 			
 			if(rows.length == 0){
 				$scope.notes.push( { texto: "No hay notas para mostrar..." } );
